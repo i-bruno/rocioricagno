6746433d444160039b1756e945765cda4a7f543c
Ignacio Bruno <ignaciobruno90@gmail.com>
2023-12-08 12:25:24 -0300

    Se estableció un nuevo inicio con una galería de obras

--- src/components/Construccion/Construccion.jsx
+++ src/components/Construccion/Construccion.jsx
@@ -5,11 +5,26 @@ import Redes from '../Redes/Redes'
 const Construccion = () => {
     return (
         <div className='mensaje'>
-            <img src="./img/web/manglar.jpg" alt="obra" className='imgInicio' />
-            <h1 className='construccion'>
-                Rocío Ricagno
-            </h1>
-            <Redes />
+            <div className='grid-gallery'>
+                <div className='image'>
+                    <img src="./img/obras/03_capasAbrigo/ejercicio1.jpg" alt="obra" className='grid-gallery__image' />
+                </div>
+                <div className='image'>
+                    <img src="./img/obras/04_pielDeRio/pielDeRio1.jpg" alt="" className='grid-gallery__image' />
+                </div>
+                <div className='image'>
+                    <img src="./img/obras/05_sintesisMemoria/sinTitulo.jpg" alt="" className='grid-gallery__image' />
+                </div>
+                <div className='image'>
+                    <img src="./img/obras/06_memoriaCartografica/memoriaCartografica.jpg" alt="" className='grid-gallery__image' />
+                </div>
+                <div className='image'>
+                    <img src="./img/obras/07_inestable/inestable.jpg" alt="" className='grid-gallery__image' />
+                </div>
+                <div className='image'>
+                    <img src="./img/obras/08_devastacion/devastacionCompleta.jpg" alt="" className='grid-gallery__image' />
+                </div>
+            </div>
         </div>
     )
 }
