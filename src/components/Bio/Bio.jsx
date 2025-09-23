41a9464cbccd15619c2f670e5f1cba4d2af0870c
Ignacio Bruno <ignaciobruno90@gmail.com>
2025-09-22 23:19:56 -0300

    Se actualiza el statement

--- src/components/Bio/Bio.jsx
+++ src/components/Bio/Bio.jsx
@@ -10,20 +10,23 @@ const Bio = () => {
                         <img src="./img/web/perfil1.webp" alt="foto de perfil" className='imgPerfil' />
                         <div className="bioText">
                             <p>
-                                Nace en el año 1990 en San Fernando, provincia de Buenos Aires. Crece en Tigre y se reconoce tanto parte de su ciudad como de sus islas. Es artista visual y docente de la Ciudad de Buenos Aires. Sus prácticas artísticas se vinculan con el delta del río Paraná, donde utiliza diversos lenguajes y acciones para generar nuevas miradas en torno al territorio. Recibe en 2015 el título de Profesora de Artes Visuales otorgado por la Escuela de Artes Visuales Antonio Berni.
+                                Rocío Ricagno nace en el año 1990 en San Fernando, Buenos Aires. Crece en Tigre y se reconoce tanto parte de su ciudad como de sus islas. Es artista visual y docente. Sus prácticas artísticas se vinculan con el delta del río Paraná, donde utiliza diversos lenguajes y acciones para generar nuevas miradas en torno al territorio.
                             </p>
-                            {/* <p>
+                            <p className='noDisplay'>
                                 En 2016 se traslada a la Capital Federal y cursa la especialización en Educación Sexual Integral en el Inst. del Profesorado Dr. Joaquín V. González. Desde su rol docente colabora en la muestra itinerante “No Fue un Juego” en el Museo del Holocausto, Bs. As. 2018/2019. Lleva a cabo junto con el equipo educativo del Ex CCD,TyE “Club Atlético” la muestra “Gol a la memoria” A cuarenta años del mundial ‘78 - 2018.
-                            </p> */}
+                            </p>
                             <p>
-                                Estudia Fotografía Documental en FILO - UBA y Cine Documental Comunitario en la ENERC - 2020. Realiza Clínica de obra con Andrés Labaké, María Lightowler y Federico de La Puente. Actualmente asiste a los talleres de Diego Perrotta y “Explorar la trama” de Emilia Demichelis. En 2021 y 2022 accede a becas de residencia, tales como “La Rural”, en Tucumán, “Interzona”, en la ciudad de La Plata y “SirveVerse” de La Paternal Espacio Proyecto en Entre Ríos.
+                                Recibe en 2015 el título de Profesora de Artes Visuales otorgado por la Escuela de Artes Visuales Antonio Berni. Estudia Fotografía Documental en FILO - UBA y Cine Documental Comunitario en la ENERC - 2020. Hoy en día cursa el ciclo de licenciatura en Artes de la UNSAM. Realiza Clínica de obra con Andrés Labaké, María Lightowler y Federico de La Puente. Entre el 2020 y el 2023 asiste a los talleres de Emilia Demichelis y Diego Perrotta. 
                             </p>
                             <div className='bioText2'>
                                 <p>
-                                    Es seleccionada para cursar el programa anual de estudios de creación y circulación de obra en Proyecto Imaginario LATAM y le fue otorgada la Beca Creación del Fondo Nacional de las Artes en el año 2021 y 2022. Expone en diversos espacios de la Ciudad y la Provincia de Buenos Aires. Recibe 1ra Mención en el 4to salón de la mujer: Género y Derechos del Centro Argentino de Arte Textil y Mención en el 5to Premio MacSur 2022. Actualmente el Palacio Belgrano Otamendi de la Ciudad de San Fernando adquirió una de sus obras para que forme parte de su muestra permanente en el museo.
+                                    En el 2021 y 2022 le otorgan becas de residencia en Tucumán, Entre Ríos y Buenos Aires. Participó en variados salones, muestras colectivas y realizó muestras individuales en Neuquén, Córdoba, Santa Fe y Buenos Aires. Es seleccionada para cursar el programa anual de estudios de creación y circulación de obra en Proyecto Imaginario LATAM y le fue otorgada la Beca Creación del FNA en el año 2021 y 2022. 
                                 </p>
                                 <p>
-                                    Actualmente se encuentra explorando las características de los materiales y sus poéticas. A su vez, ampliando las fronteras de expresión incorporando publicaciones gráficas, sitios web, fotografía, video, performance, el registro sonoro y lo instalativo.
+                                    Recibe 1ra Mención en el 4to salón de la mujer: Género y Derechos del Centro Argentino de Arte Textil y Mención en el 5to Premio MacSur 2021. En el 2023 el Palacio Belgrano Otamendi de la Ciudad de San Fernando adquirió una de sus obras para que forme parte de su muestra permanente en el museo.
+                                </p>
+                                <p>
+                                    Actualmente se encuentra explorando las características de los materiales y sus poéticas. A su vez, ampliando las fronteras de expresión incorporando publicaciones gráficas, medios audiovisuales y lo instalativo.
                                 </p>
                             </div>
                         </div>
@@ -138,28 +141,6 @@ const Bio = () => {
                     </div>
                 </section>
             </div>
-
-
-            {/* <div className='d-flex d-row'>
-                <section>
-                    <p>
-                        Soy artista visual y docente de la Ciudad de Buenos Aires. Mis prácticas artísticas se vinculan con el delta del río Paraná, donde utilizo diversos lenguajes y acciones para generar nuevas miradas en torno al territorio. Recibí en 2015 el título de Profesora de Artes Visuales otorgado por la Escuela de Artes Visuales Antonio Berni.
-                    </p>
-                    <p>
-                        En 2016 me trasladé a la Capital Federal donde realicé la especialización en Educación Sexual Integral en el Inst. del Profesorado Dr. Joaquín V. González. Desde mi rol docente colaboré en la muestra itinerante “No Fue un Juego” en el Museo del Holocausto, Bs. As. 2018/2019 y Llevé a cabo junto con el equipo educativo del Ex CCD,TyE “Club Atlético” la muestra “Gol a la memoria” A cuarenta años del mundial ‘78 - 2018.
-                    </p>
-                    <p>
-                        Estudié Fotografía Documental en FILO - UBA y Cine Documental Comunitario en la ENERC - 2020. Realicé Clínica de obra con Andrés Labaké, María Lightowler y Federico de La Puente. Actualmente asisto a los talleres de Diego Perrotta y de Emilia Demichelis. En 2021 y 2022 accedí a becas de residencia, tales como “La Rural”, en Tucumán, “Interzona”, en la ciudad de La Plata y “SirveVerse” de La Paternal Espacio Proyecto en Entre Ríos. 
-                    </p>
-                    <p>
-                        Fuí seleccionada para cursar el programa anual de estudios de creación y circulación de obra en Proyecto Imaginario Latinoamérica y me fue otorgada la Beca Creación del Fondo Nacional de las Artes en el año 2021 y 2022. Expuse en diversos espacios de la Ciudad y la Provincia de Buenos Aires. Recibí 1ra Mención en el 4to salón de la mujer: Género y Derechos del Centro Argentino de Arte Textil y Mención en el 5to Premio MacSur 2022. Actualmente el Palacio Belgrano Otamendi de la Ciudad de San Fernando adquirió una de mis obras para que forme parte de su muestra permanente en el museo. 
-                    </p>
-                </section>
-
-                <section>
-
-                </section>
-            </div> */}
         </>
     )
 }
